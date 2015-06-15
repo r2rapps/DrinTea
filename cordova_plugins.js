a1a5ad18b46addb2e5bbabd0d7ff513a154d1815
r2rapps <r2rapps@gmail.com>
2015-06-16 05:41:29 +0800

    MAin
    Main

---
+++ cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/de.appplant.cordova.plugin.email-composer/www/email_composer.js",
+        "id": "de.appplant.cordova.plugin.email-composer.EmailComposer",
+        "clobbers": [
+            "cordova.plugins.email",
+            "plugin.email"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.0",
+    "de.appplant.cordova.plugin.email-composer": "0.8.2"
+}
+// BOTTOM OF METADATA
+});
